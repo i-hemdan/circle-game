c5fd2dc14007f56a49a95a35607d92cb1b25df1c
i hemdan <i_hemdan@hotmail.com>
2018-09-16 13:30:07 -0600

    initial commit

---
+++ CircleGame.js
@@ -0,0 +1,71 @@
+
+
+var gameCircle = function(position_x,position_y,radius, speed){
+    this.speed = speed;
+    this.position_x = position_x;
+    this.position_y = position_y;
+    this.radius = radius;
+}
+gameCircle.prototype.move = function(x,y){
+    this.position_x += x;
+    this.position_y += y;
+}
+
+function handleGameInput(circle){
+    if(keyIsDown(LEFT_ARROW)){
+        circle.move(-circle.speed, 0);
+    }
+    if(keyIsDown(RIGHT_ARROW)){
+        circle.move(circle.speed, 0);
+    }
+    if(keyIsDown(UP_ARROW)){
+        circle.move(0,-circle.speed);
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        circle.move(0,circle.speed);
+    }
+}
+
+function generateCircle(maxRadius){
+    var r = random(5, maxRadius - 1);
+    var x = random(width);
+    var y = random(height);
+    return new gameCircle(x,y,r,0);
+}
+
+function circleGenerationTimer(interval){
+    this.baseInterval = interval;
+    this.curInterval = interval;
+}
+circleGenerationTimer.prototype.elapse = function(){
+    if(millis() > this.curInterval) {
+        this.curInterval += this.baseInterval;
+        return true;
+    }
+    return false;
+}
+var genTimer = new circleGenerationTimer(1000);
+var playerCircle = new gameCircle(10,10,20,5);
+var circleFood = [];
+
+function setup() {
+    createCanvas(640,480);
+    frameRate(30);
+}
+
+function draw() {
+    background(100);
+    handleGameInput(playerCircle);
+    fill(0);
+    ellipse(playerCircle.position_x, playerCircle.position_y, playerCircle.radius*2,playerCircle.radius*2);
+    if(genTimer.elapse()){
+        circleFood.push(generateCircle(playerCircle.radius));
+    }
+
+    fill(255,0,0);
+    if(circleFood.length > 0){
+        for(let i = 0; i < circleFood.length; i++){
+            ellipse(circleFood[i].position_x, circleFood[i].position_y, circleFood[i].radius*2, circleFood[i].radius*2);
+        }
+    }
+}
